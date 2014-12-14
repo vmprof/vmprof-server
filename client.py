95581d89aa664c19aee7f67c5f5f4b52a913c692
xando <sebastian.pawlus@gmail.com>
2014-12-14 11:05:21 +0100

    renamed

--- client.py
+++ client.py
@@ -11,9 +11,10 @@ prof = base64.b64encode(open(sys.argv[1], 'rb').read())
 prof_sym = base64.b64encode(open(sys.argv[2], 'rb').read())
 
 
-response = requests.post("http://127.0.0.1:8000/submit/", data={
+response = requests.post("http://127.0.0.1:8000/api/log/", data={
     "prof": prof,
     "prof_sym": prof_sym
 })
 
-print response.content
+
+print response.json()
