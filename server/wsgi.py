2373d6b266e60f8e1d7d5f53d28bc191f522181b
xando <sebastian.pawlus@gmail.com>
2015-01-03 16:59:25 +0100

    cleaning

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,11 +1,9 @@
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+# import os
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+# from vmprof import DjangoVMPROF
+# vmprof = DjangoVMPROF("localhost", 8000, "token")
+# app = vmprof(get_wsgi_application())
 
 from django.core.wsgi import get_wsgi_application
-# from vmprof import DjangoVMPROF
-
-
-# vmprof = DjangoVMPROF("localhost", 8000, "token")
-
-# app = vmprof(get_wsgi_application())
 app = get_wsgi_application()
