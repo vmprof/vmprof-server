905742fc29b5dd20bab06d44b18fb49a297e7b25
xando <sebastian.pawlus@gmail.com>
2015-01-15 17:21:46 +0100

    fix

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,7 @@ import uuid
 
 from fabric.api import *
 from fabric.contrib import files
+from fabric.context_managers import shell_env
 
 
 PATH = "/var/www/vmprof"
@@ -38,7 +39,8 @@ def deploy():
 
         run("mkdir -p vmprof/static")
 
-        run("%s vmprof/manage.py collectstatic -c --noinput" % ENV_PYTHON)
-        run("%s vmprof/manage.py migrate" % ENV_PYTHON)
+        with shell_env(DJANGO_SETTINGS_MODULE='settings.production'):
+            run("%s vmprof/manage.py collectstatic -c --noinput" % ENV_PYTHON)
+            run("%s vmprof/manage.py migrate" % ENV_PYTHON)
 
         run("%s --ini vmprof/uwsgi.ini" % ENV_UWSGI)
