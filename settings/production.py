4f0fc9afa1d39cef3a38995bdea9b2e273e6bdb8
xando <sebastian.pawlus@gmail.com>
2015-01-15 16:58:32 +0100

    production data base moved

--- settings/production.py
+++ settings/production.py
@@ -3,3 +3,10 @@ from . import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
+    }
+}
