f9aa25d8660defb5c2ee1008f5fe5b268979c905
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2015-03-25 18:04:10 +0100

    logging moved to production

--- settings/production.py
+++ settings/production.py
@@ -10,3 +10,23 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
     }
 }
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, '..', 'vmprof.log'),
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
