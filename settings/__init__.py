0286b6373d37eb2569acb7c8ec772015b52a9fb5
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2015-03-25 18:02:29 +0100

    simple debug

--- settings/__init__.py
+++ settings/__init__.py
@@ -50,3 +50,23 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'vmprof.log'),
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
