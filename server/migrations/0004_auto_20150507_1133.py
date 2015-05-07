36dd15d1011d4d56da78a047ac3fc393e3a7b6ff
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2015-05-07 13:39:57 +0200

    assing default user to log

---
+++ server/migrations/0004_auto_20150507_1133.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+def asssing_default_user(apps, schema_editor):
+
+    Log = apps.get_model("server", "Log")
+    User = apps.get_model("auth", "User")
+
+    if User.objects.count() > 0:
+        user = User.objects.all()[0]
+    else:
+        user = User.objects.create_user(
+            "anonymous",
+            "anonymous@email.com",
+            "thepassword"
+        )
+
+    Log.objects.filter(user__isnull=True).update(user=user)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('server', '0003_log_secret'),
+    ]
+
+    operations = [
+        migrations.RunPython(asssing_default_user),
+    ]
