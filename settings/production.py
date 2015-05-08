51bacabf8afa32295c1d94d7d911317d1f2b10f0
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2015-05-08 17:35:01 +0200

    sentry added to stack

--- settings/production.py
+++ settings/production.py
@@ -12,6 +12,15 @@ DATABASES = {
 }
 
 
+INSTALLED_APPS = INSTALLED_APPS + (
+    'raven.contrib.django.raven_compat',
+)
+
+# this is just for test
+RAVEN_CONFIG = {
+    'dsn': 'https://68b9d0f79fde48d5a1b0a155521d07d5:6ba8f1aa58e342ba91811047538db625@app.getsentry.com/43517',
+}
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
