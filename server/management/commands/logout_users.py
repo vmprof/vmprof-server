3c9cd0820e159b4899ed57cbeaa2ade5b86b9ed4
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2015-05-12 11:37:22 +0200

    clear users sessions command

---
+++ server/management/commands/logout_users.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from django.contrib.sessions.models import Session
+
+
+class Command(BaseCommand):
+    help = 'Clears users sessions'
+
+    def handle(self, *args, **options):
+        # XXX a bit too dramatic, but will work for now
+        
+        Session.objects.all().delete()
+        print "done"
