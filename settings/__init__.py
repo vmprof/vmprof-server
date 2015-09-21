b0e58ad826d25a109216c280b24f17ac72cc15de
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2015-09-21 15:02:15 +0200

    size of the paggination

--- settings/__init__.py
+++ settings/__init__.py
@@ -64,5 +64,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 15
+    'PAGE_SIZE': 10
 }
