9163b27d8b0c4989bfe3b6bd3c98e34450f1ff15
Richard Plangger <planrichi@gmail.com>
2016-07-15 16:33:51 -0500

    comment out not finished test

--- vmlog/test/e2e/test_visual_trace.py
+++ vmlog/test/e2e/test_visual_trace.py
@@ -7,9 +7,9 @@ from vmlog.test.e2e.test_traces import (query1, query,
         reset_search_criteria, select_trace_entry, _url)
 
 class TestTracesView(object):
-    def test_(self, drivers):
-        for driver in drivers:
-            wait = ui.WebDriverWait(driver,10)
-            driver.get(_url("#/1v1/traces"))
-            wait.until(lambda d: not query1(d, '#loading_img').is_displayed())
-            select_trace_entry(driver, wait, "")
+    #def test_(self, drivers):
+    #    for driver in drivers:
+    #        wait = ui.WebDriverWait(driver,10)
+    #        driver.get(_url("#/1v1/traces"))
+    #        wait.until(lambda d: not query1(d, '#loading_img').is_displayed())
+    #        select_trace_entry(driver, wait, "")
