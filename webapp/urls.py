b90e7b505a68f96049b65decd1697a6b6167fbef
Richard Plangger <planrichi@gmail.com>
2016-09-07 11:17:31 +0200

    url must not end with a slash, when using -m jitlog upload. it seems that some part strips off the suffix slash (some times)

--- webapp/urls.py
+++ webapp/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(router.urls)),
     url(r'^api/user/', MeView.as_view()),
-    url(r'^api/jitlog/(?P<profile>[0-9a-z]*)/$', BinaryJitLogFileUploadView.as_view()),
+    url(r'^api/jitlog/(?P<profile>[0-9a-z]*)/?$', BinaryJitLogFileUploadView.as_view()),
     url(r'^api/log/meta/(?P<profile>[0-9a-z]*)/$', meta),
     url(r'^api/log/trace/(?P<profile>[0-9a-z]*)/$', trace),
     url(r'^api/log/stitches/(?P<profile>[0-9a-z]*)/$', stitches),
